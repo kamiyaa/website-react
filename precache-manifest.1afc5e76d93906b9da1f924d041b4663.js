71982b2d0dfe6eb4c77452ed29fc99059567f7f6
Jiayi Zhao <jeff.no.zhao@gmail.com>
2019-11-09 17:28:44 -0500

    Updates

---
+++ precache-manifest.1afc5e76d93906b9da1f924d041b4663.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "760aa90e0791f92346c891eed540b8a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "693ea35534ef67270318",
+    "url": "/static/css/main.7dbb6f64.chunk.css"
+  },
+  {
+    "revision": "debe148e793cec4b7998",
+    "url": "/static/js/2.40689de5.chunk.js"
+  },
+  {
+    "revision": "693ea35534ef67270318",
+    "url": "/static/js/main.9d55a7a2.chunk.js"
+  },
+  {
+    "revision": "d194fdb4740176636e87",
+    "url": "/static/js/runtime-main.6b629bf8.js"
+  },
+  {
+    "revision": "e46e364f8f95d98560a7434e6ff23ed8",
+    "url": "/static/media/bg.e46e364f.jpg"
+  }
+]);
