16517dcc3b11404f3e5ce05e2ef284e4af971640
Jiayi Zhao <jeff.no.zhao@gmail.com>
2020-04-17 19:15:08 -0400

    Updates

---
+++ precache-manifest.36f3226687f9077eb430cd94a5264710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c12b5425a8058ac07d1fb3ba404a93a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c22e08cd6491e724fee",
+    "url": "/static/css/main.b6a369cd.chunk.css"
+  },
+  {
+    "revision": "24343179691a77d70605",
+    "url": "/static/js/2.6dbdabf0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6dbdabf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c22e08cd6491e724fee",
+    "url": "/static/js/main.6e3251fb.chunk.js"
+  },
+  {
+    "revision": "e0b4e5a69bafcfe2b804",
+    "url": "/static/js/runtime-main.0e3ad261.js"
+  }
+]);
